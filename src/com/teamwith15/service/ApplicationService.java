c683f0db46b4d86769e1db8778b8d7ae6b1ea687
killop2000 <killop2000@naver.com>
2018-08-04 20:48:22 +0900

    asd

--- src/com/teamwith15/service/ApplicationService.java
+++ src/com/teamwith15/service/ApplicationService.java
@@ -27,6 +27,7 @@ import com.teamwith15.vo.MemberSearchVO;
 import com.teamwith15.vo.MyApplicationVO;
 
 public class ApplicationService {
+	private String teamwith;
 	private static ApplicationService applicationService;
 	static {
 		applicationService=new ApplicationService();
